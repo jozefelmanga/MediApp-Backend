92455150af0de6e9d9d2cf9d50fe851846d5b6fc
Youssef Ben Salem <youssefbselem@gmail.com>
2025-12-04 13:21:16 +0100

    feat: initialize notification and user services with Spring Boot
    - Created `notification-service` module with necessary dependencies and configurations.
    - Added main application class `NotificationServiceApplication` for the notification service.
    - Configured application properties for the notification service.
    - Implemented basic test class `NotificationServiceApplicationTests` for the notification service.
    
    - Created `user-service` module with necessary dependencies and configurations.
    - Added main application class `UserServiceApplication` for the user service.
    - Configured application properties for the user service.
    - Implemented basic test class `UserServiceApplicationTests` for the user service.
    
    - Added Maven wrapper scripts and properties for both services.
    - Configured `.gitignore` and `.gitattributes` for proper version control.

---
+++ user-service/src/main/java/com/mediapp/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mediapp.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
