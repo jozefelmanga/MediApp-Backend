1af6a999d6ac049d0ddce1a5cce80933e72a38d5
Youssef Ben Salem <youssefbselem@gmail.com>
2025-12-04 14:41:31 +0100

    feat: Update database configuration to use MySQL and add test profile settings

--- security-service/src/test/java/com/mediapp/security_service/SecurityServiceApplicationTests.java
+++ security-service/src/test/java/com/mediapp/security_service/SecurityServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.mediapp.security_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "eureka.client.enabled=false")
+@ActiveProfiles("test")
 class SecurityServiceApplicationTests {
 
 	@Test
